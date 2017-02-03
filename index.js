c94f06f889e1f9a696edebbe3efa63211d6dd4ef
Manoj Chalode <chalodem@gmail.com>
2017-02-03 13:38:46 +0000

    added first directive

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-(function () {
-	
-	'use strict';
+(function() {
 
-	console.log('yes!');
+    'use strict';
 
+    angular.module('angular-demo', [])
+        .controller('MainController', ['$scope', function($scope) {
+            $scope.name = "Manoj Chalode";
+            $scope.address = "Flat 30/4, Roeburn Hall, Preston PR2 2QQ";
+        }])
+        .directive('nameDirective', function() {
+            return {
+                templateUrl: function (element, attr) {
+                	return 'templates/'+ attr.address +'-template.html';
+                },
+                restrict: 'AE'
+            };
+        });
 })();
